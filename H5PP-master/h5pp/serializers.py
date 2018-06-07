60cb51e16c0022813476cd14fc271955fb40acfc
saurabhshwetabhsingh <saurabhshwetabhsingh@gmail.com>
2018-06-07 16:24:44 +0530

    UI modifications and upload option working properly

--- H5PP-master/h5pp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import h5p_contents
-
-class h5pcontentSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = h5p_contents
-		fields = ('content_id','title', 'community_id', 'community_name')
--- H5PP-master/h5pp/static/assets/plugins/script.js
+++
@@ -1,9 +0,0 @@
-(function($){
-$(document).ready(function ($) {
-    $('.timer').countTo();
-    $('.counter-item').appear(function() {
-        $('.timer').countTo();
-    },{accY: -100});
-});
-
-}(jQuery));
